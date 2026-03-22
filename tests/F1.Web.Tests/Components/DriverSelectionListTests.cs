ae3cae995453b716ec94864f4db64cc8ce4b0a85
Philip Woulfe <philip.woulfe@gmail.com>
2026-03-22 20:52:40 +0000

    fix(web): harden selection components for required ValueExpression and safe driver list normalization

--- tests/F1.Web.Tests/Components/DriverSelectionListTests.cs
+++ tests/F1.Web.Tests/Components/DriverSelectionListTests.cs
@@ -6,6 +6,17 @@ namespace F1.Web.Tests.Components;
 public class DriverSelectionListTests : BunitContext
 {
     [Fact]
+    public void DriverSelectionList_ShouldRender_WhenSelectedDriverIdsIsNotProvided()
+    {
+        var cut = Render<DriverSelectionList>(parameters => parameters
+            .Add(p => p.Drivers, Array.Empty<Driver>())
+            .Add(p => p.IsReadOnly, false));
+
+        Assert.Equal(5, cut.FindAll("select").Count);
+        Assert.Equal(5, cut.Instance.SelectedDriverIds.Count);
+    }
+
+    [Fact]
     public void DriverSelectionList_ShouldRenderFiveSelectors_WithDriverOptions()
     {
         var selectedDriverIds = new List<string> { "", "", "", "", "" };
@@ -58,5 +69,33 @@ public class DriverSelectionListTests : BunitContext
             .Add(p => p.IsReadOnly, true));
 
         Assert.True(cut.FindAll("select").All(element => element.HasAttribute("disabled")));
+    }
+
+    [Fact]
+    public void DriverSelectionList_ShouldPadSelectedDriverIds_WhenShortListProvided()
+    {
+        var selectedDriverIds = new List<string> { "norris", "leclerc" };
+
+        var cut = Render<DriverSelectionList>(parameters => parameters
+            .Add(p => p.Drivers, Array.Empty<Driver>())
+            .Add(p => p.SelectedDriverIds, selectedDriverIds)
+            .Add(p => p.IsReadOnly, false));
+
+        Assert.Equal(5, cut.Instance.SelectedDriverIds.Count);
+        Assert.Equal("norris", cut.Instance.SelectedDriverIds[0]);
+        Assert.Equal("leclerc", cut.Instance.SelectedDriverIds[1]);
+        Assert.Equal(string.Empty, cut.Instance.SelectedDriverIds[2]);
+    }
+
+    [Fact]
+    public void DriverSelectionList_ShouldNormalizeSelectedDriverIds_WhenNullProvided()
+    {
+        var cut = Render<DriverSelectionList>(parameters => parameters
+            .Add(p => p.Drivers, Array.Empty<Driver>())
+            .Add(p => p.SelectedDriverIds, (IList<string>)null!)
+            .Add(p => p.IsReadOnly, false));
+
+        Assert.Equal(5, cut.FindAll("select").Count);
+        Assert.Equal(5, cut.Instance.SelectedDriverIds.Count);
     }
 }
